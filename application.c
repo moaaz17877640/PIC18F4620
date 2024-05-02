d7c6909e7ca1372ee229e9ce013d36cc0dbb7d63
moaaz <mstfyrb447@gmail.com>
2024-05-02 18:23:36 +0300

    first commit

---
+++ application.c
@@ -0,0 +1,19 @@
+/*
+ * File:   application.c
+ * Author: Moaaz elmahi
+ *
+ * Created on December 1, 2023, 10:21 PM
+ */
+
+
+#include "application.h"
+Std_ReturnType ret= E_NOT_OK;
+int main() {
+
+ while(1){
+
+ }
+
+    return (EXIT_SUCCESS);
+
+}
